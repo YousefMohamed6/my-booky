d29df9ea912ebbfb2d4284d1a51f3c70f56609ef
Yousef Mohamed <yo7sef2002@gmail.com>
2023-09-06 21:44:19 +0300

    solve some bugs

--- lib/features/splash/views/widgets/splash_view_body.dart
+++ lib/features/splash/views/widgets/splash_view_body.dart
@@ -2,7 +2,6 @@ import 'package:bookly/constants.dart';
 import 'package:bookly/core/widgets/vertical_sizebox.dart';
 import 'package:bookly/features/home/views/home_view.dart';
 import 'package:bookly/features/splash/views/widgets/slider_text.dart';
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -45,10 +44,10 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 250),
+      duration: const Duration(seconds: 1),
     );
     slidingAnimation =
-        Tween<Offset>(begin: const Offset(0.0, 20.0), end: Offset.zero)
+        Tween<Offset>(begin: const Offset(0.0, 10.0), end: Offset.zero)
             .animate(animationController);
     animationController.forward();
   }
@@ -57,8 +56,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
     Future.delayed(
       const Duration(seconds: 3),
       () {
-        Get.to(() => const HomeView(),
-            transition: Transition.fade, duration: kTransitonDuration);
+        Navigator.pushNamed(context, HomeView.id);
       },
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/features/home/views/home_view.dart';
+import 'package:bookly/features/splash/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +12,11 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
+      routes: {
+        HomeView.id: (context) => const HomeView(),
+      },
+      home: const SplashView(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimryColor),
     );
