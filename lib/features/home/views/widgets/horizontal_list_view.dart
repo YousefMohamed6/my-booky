246207463f8b473305c0171a7cb6347ff89e8b25
Yousef Mohamed <yo7sef2002@gmail.com>
2023-09-07 01:52:48 +0300

    save

--- lib/features/home/views/widgets/horizontal_list_view.dart
+++ lib/features/home/views/widgets/horizontal_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/widgets/vertical_sizebox.dart';
 import 'package:bookly/features/home/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
@@ -5,7 +6,8 @@ class HorizontalListView extends StatelessWidget {
   const HorizontalListView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return CustomSizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: 5,
