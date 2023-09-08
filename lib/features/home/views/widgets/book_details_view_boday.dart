cb850df852ef99098e9d9652be7da39c9441782a
Yousef Mohamed <yo7sef2002@gmail.com>
2023-09-08 13:41:49 +0300

    add book data widget in book detail view

--- lib/features/home/views/widgets/book_details_view_boday.dart
+++ lib/features/home/views/widgets/book_details_view_boday.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/widgets/custom_text.dart';
 import 'package:bookly/core/widgets/vertical_sizebox.dart';
 import 'package:bookly/features/home/views/widgets/book_appbar.dart';
 import 'package:bookly/features/home/views/widgets/custom_book_item.dart';
+import 'package:bookly/features/home/views/widgets/rating.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,13 +13,38 @@ class BookDetailsViewBody extends StatelessWidget {
     return Column(
       children: [
         const BookAppBar(),
-        const CustomSizedBox(height: 16),
+        const CustomSizedBox(height: 8),
         Padding(
           padding: EdgeInsets.symmetric(
-            horizontal: MediaQuery.of(context).size.width * 0.17,
+            horizontal: MediaQuery.of(context).size.width / 8,
           ),
           child: const CustomBookItem(),
         ),
+        const BookData(),
+      ],
+    );
+  }
+}
+
+class BookData extends StatelessWidget {
+  const BookData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CustomText(
+          text: 'The Jungle Book',
+          fontSize: 32,
+        ),
+        CustomText(
+          text: 'Rudyard Kipling',
+          fontSize: 24,
+        ),
+        CustomSizedBox(height: 4),
+        Rating(),
       ],
     );
   }
--- lib/features/home/views/widgets/custom_book_item.dart
+++ lib/features/home/views/widgets/custom_book_item.dart
@@ -8,7 +8,7 @@ class CustomBookItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomSizedBox(
-      height: MediaQuery.of(context).size.height * .3,
+      height: MediaQuery.of(context).size.height * 0.3,
       child: AspectRatio(
         aspectRatio: 2.7 / 4,
         child: Container(
