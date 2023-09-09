1bbb6524cb51696fad24a7191c9a2b69c7eff077
Yousef Mohamed <yo7sef2002@gmail.com>
2023-09-09 19:03:44 +0300

    implement fetch use books

--- lib/features/home/data/repo/home_repo_implementation.dart
+++ lib/features/home/data/repo/home_repo_implementation.dart
@@ -1,19 +1,31 @@
 import 'package:bookly/core/error/failure.dart';
+import 'package:bookly/core/uilts/api_service.dart';
 import 'package:bookly/features/home/data/model/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
 
 class HomeRepoImplemntation implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImplemntation(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    Dio().get('');
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items'][0]) {
+        books.add(BookModel.fromJsonData(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(e);
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    
     throw UnimplementedError();
   }
 }
